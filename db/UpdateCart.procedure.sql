baf06bcdc51610a90e0c7ddf30c760a697f553e9
Vishwesh32 <v.d.chotaliya30@gmail.com>
2021-06-18 18:40:39 +0530

    Changes

--- db/UpdateCart.procedure.sql
+++ db/UpdateCart.procedure.sql
@@ -3,9 +3,9 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `UpdateCart`(IN `_UserID` INT(11), I
 BEGIN
 	DECLARE checkCount INT;
 	SET checkCount = (SELECT COUNT(*)
- FROM cart WHERE UserID = _UserID AND ProductID = _ProductID);
+ 	FROM cart WHERE UserID = _UserID AND ProductID = _ProductID);
    
-   CASE
+    CASE
 	WHEN _Quantity <= 0 THEN DELETE FROM cart WHERE UserID = _UserID AND ProductID = _ProductID;
 	ELSE UPDATE cart SET Quantity = _Quantity WHERE UserID = _UserID AND ProductID = _ProductID;
     UPDATE cart INNER JOIN product on product.ProductID = cart.ProductID SET cart.Total = product.Price * cart.Quantity where cart.UserID = _UserID AND cart.ProductID = _ProductID;
