d3e2ccd88f71fea9e2d7b37a4192a4994af7f3e9
Vishwesh32 <v.d.chotaliya30@gmail.com>
2021-06-25 17:55:43 +0530

    Toggle Password Visibility

--- views/js/app.js
+++ views/js/app.js
@@ -1,10 +1,28 @@
-const togglePassword = document.querySelector('#password');
-const password = document.querySelector('#New_pass');
+// // const toggleOldPassword = document.querySelector('#toggleOldPassword');
+// // const password1 = document.querySelector('#Old_pass');
 
-togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye / eye slash icon
-    this.classList.toggle('bi-eye');
-});
+// // toggleOldPassword.addEventListener('click', function (e) {
+// //     // toggle the type attribute
+// //     const type = password1.getAttribute('type') === 'password' ? 'text' : 'password';
+// //     password1.setAttribute('type', type);
+// // });
+
+
+// const toggleNewPassword = document.querySelector('#toggleNewPassword');
+// const password2 = document.querySelector('#New_pass');
+
+// toggleNewPassword.addEventListener('click', function (e) {
+//     // toggle the type attribute
+//     const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+//     password2.setAttribute('type', type);
+// });
+
+// const toggleConfirmPassword = document.querySelector('#toggleConfirmPassword');
+// const password3 = document.querySelector('#Confirm_pass');
+
+// toggleConfirmPassword.addEventListener('click', function (e) {
+//     // toggle the type attribute
+//     const type = password3.getAttribute('type') === 'password' ? 'text' : 'password';
+//     password3.setAttribute('type', type);
+    
+// });
